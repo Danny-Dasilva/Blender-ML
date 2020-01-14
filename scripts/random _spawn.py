01f188ef6a67c66f0d6822349c546557d35c17e5
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-01-13 21:00:13 -0500

    added random spawn

---
+++ scripts/random _spawn.py
@@ -0,0 +1,295 @@
+import bpy
+import mathutils
+from random import randint, uniform
+from math import *
+import os
+import json
+from mathutils import Vector, Quaternion
+from time import sleep 
+
+
+
+import time
+import warnings
+#from scripts import PID
+
+
+import time
+import warnings
+
+
+
+def update():
+    dg = bpy.context.evaluated_depsgraph_get() 
+    dg.update()
+
+import numpy as np
+
+def camera_view_bounds_2d(scene, camera_object, mesh_object):
+    """
+    Returns camera space bounding box of the mesh object.
+    Gets the camera frame bounding box, which by default is returned without any transformations applied.
+    Create a new mesh object based on mesh_object and undo any transformations so that it is in the same space as the
+    camera frame. Find the min/max vertex coordinates of the mesh visible in the frame, or None if the mesh is not in view.
+    :param scene:
+    :param camera_object:
+    :param mesh_object:
+    :return:
+    """
+
+    """ Get the inverse transformation matrix. """
+    matrix = camera_object.matrix_world.normalized().inverted()
+    """ Create a new mesh data block, using the inverse transform matrix to undo any transformations. """
+    dg = bpy.context.evaluated_depsgraph_get()
+    
+    ob = mesh_object.evaluated_get(dg) #this gives us the evaluated version of the object. Aka with all modifiers and deformations applied.
+    mesh = ob.to_mesh()
+    #mesh = mesh_object.to_mesh()
+    mesh.transform(mesh_object.matrix_world)
+    mesh.transform(matrix)
+
+    """ Get the world coordinates for the camera frame bounding box, before any transformations. """
+    frame = [-v for v in camera_object.data.view_frame(scene=scene)[:3]]
+
+
+    lx = []
+    ly = []
+    
+    for v in mesh.vertices:
+        co_local = v.co
+        z = -co_local.z
+
+        if z <= 0.0:
+            """ Vertex is behind the camera; ignore it. """
+            continue
+        else:
+            """ Perspective division """
+            frame = [(v / (v.z / z)) for v in frame]
+        
+        min_x, max_x = frame[1].x, frame[2].x
+        min_y, max_y = frame[0].y, frame[1].y
+        
+        x = (co_local.x - min_x) / (max_x - min_x)
+        y = (co_local.y - min_y) / (max_y - min_y)
+        lx.append(x)
+        ly.append(y)
+    
+    mesh_object.to_mesh_clear()
+
+    """ Image is not in view if all the mesh verts were ignored """
+    if not lx or not ly:
+        return None
+
+    min_x = np.clip(min(lx), 0.0, 1.0)
+    min_y = np.clip(min(ly), 0.0, 1.0)
+    max_x = np.clip(max(lx), 0.0, 1.0)
+    max_y = np.clip(max(ly), 0.0, 1.0)
+
+    """ Image is not in view if both bounding points exist on the same side """
+    if min_x == max_x or min_y == max_y:
+        return None
+
+    """ Figure out the rendered image size """
+    render = scene.render
+    fac = render.resolution_percentage * 0.01
+    dim_x = render.resolution_x * fac
+    dim_y = render.resolution_y * fac
+
+    return (min_x, min_y), (max_x, max_y)
+
+
+
+
+
+
+
+
+def randomize_camera(x, y, z, roll=0, pitch=0, yaw=0):
+    x = y
+    y= x
+    z = z
+ 
+
+    pitch = pitch
+    roll = roll
+    yaw = randint(-yaw/2, yaw/2)
+  
+    fov = 50.0
+
+    pi = 3.14159265
+    print(bpy.data.scenes.keys())
+    scene = bpy.data.scenes['Scene']
+
+    # Set render resolution
+    scene.render.resolution_x = 640
+    scene.render.resolution_y = 640
+
+    # Set camera fov in degrees
+    scene.camera.data.angle = fov*(pi/180.0)
+
+    # Set camera rotation in euler angles
+    scene.camera.rotation_mode = 'XYZ'
+    scene.camera.rotation_euler[0] = pitch*(pi/180.0)
+    scene.camera.rotation_euler[1] = roll*(pi/180)
+    scene.camera.rotation_euler[2] = yaw*(pi/180.0)
+
+    # Set camera translation
+    scene.camera.location.x = x
+    scene.camera.location.y = y
+    scene.camera.location.z = z
+    update()
+    return scene, scene.camera
+
+
+def get_cordinates(scene, camera,  object, filename):
+    camera_object = camera
+    bounding_box = camera_view_bounds_2d(scene, camera_object, object)
+  
+    cordinates = {
+            'image': filename,
+            'meshes': {}
+        }
+    if bounding_box:
+       cordinates['meshes'][object.name] = {
+                    'x1': bounding_box[0][0],
+                    'y1': bounding_box[0][1],
+                    'x2': bounding_box[1][0],
+                    'y2': bounding_box[1][1]
+                }
+       return cordinates
+    else:
+       return None
+def measure (first, second):
+
+    locx = second[0] - first[0]
+    locy = second[1] - first[1]
+    locz = second[2] - first[2]
+
+    distance = sqrt((locx)**2 + (locy)**2 + (locz)**2) 
+    return distance
+
+
+def center_obj(obj_camera, point):
+    print(point, " [pooooiint")
+    loc_camera = obj_camera.matrix_world.to_translation()
+
+    direction = point - loc_camera
+    print(direction, "diredctr")
+    # point the cameras '-Z' and use its 'Y' as up
+    rot_quat = direction.to_track_quat('-Z', 'Y')
+    print(rot_quat, rot_quat.to_euler(), "quat")
+    # assume we're using euler rotation
+    obj_camera.rotation_euler = rot_quat.to_euler()
+    update()
+    eulers = [degrees(a) for a in obj_camera.matrix_world.to_euler()]
+    z = eulers[2]
+    print(eulers, "eulsers")
+    distance = measure(point, loc_camera)
+    return distance, z
+
+def percent_offset(distance, z, degrees):
+    fov = 50 * .9
+    width = 640
+
+    new_yaw = 640 / distance / fov
+
+    yaw = z + new_yaw
+    scene = bpy.data.scenes['_mainScene']
+    scene.camera.rotation_mode = 'XYZ'
+    scene.camera.rotation_euler[2] = yaw*(pi/180.0)
+    update()
+
+def point_at(obj, target, roll=0):
+    obj = obj.matrix_world.to_translation()
+    """
+    Rotate obj to look at target
+
+    :arg obj: the object to be rotated. Usually the camera
+    :arg target: the location (3-tuple or Vector) to be looked at
+    :arg roll: The angle of rotation about the axis from obj to target in radians. 
+
+    Based on: https://blender.stackexchange.com/a/5220/12947 (ideasman42)      
+    """
+    if not isinstance(target, mathutils.Vector):
+        target = mathutils.Vector(target)
+    #oc = obj.location
+    loc = obj
+    # direction points from the object to the target
+    direction = target - loc
+    
+    quat = direction.to_track_quat('-Z', 'Y')
+
+    # /usr/share/blender/scripts/addons/add_advanced_objects_menu/arrange_on_curve.py
+    quat = quat.to_matrix().to_4x4()
+    rollMatrix = mathutils.Matrix.Rotation(roll, 4, 'Z')
+
+    # remember the current location, since assigning to obj.matrix_world changes it
+    loc = loc.to_tuple()
+    
+    
+    obj.matrix_world = quat * rollMatrix
+    obj.location = loc
+
+
+def offset(scene, camera, angle):
+    
+    angle = angle
+    height = 640
+    width = 640
+        
+    if width > height:    
+        ratio = height / width  
+        desired_x = (50 / 2) * (angle/100) * ratio
+        desired_y = (50 / 2) * (angle/100) 
+    
+    elif height > width:
+        ratio = width / height  
+        desired_x = (50 / 2) * (angle/100)
+        desired_y = (50 / 2) * (angle/100) * ratio
+    else:
+        desired_x = (50 / 2) * (angle/100)
+        desired_y = (50 / 2) * (angle/100)
+   
+    scene.camera.rotation_mode = 'XYZ'
+    x = scene.camera.rotation_euler[0]
+    y = scene.camera.rotation_euler[2]
+    
+    change_x = x + (desired_x * (pi / 180.0))
+    change_y = y + (desired_y * (pi / 180.0))
+    #scene.camera.rotation_euler[0] = change_x 
+    #scene.camera.rotation_euler[2] = change_y 
+    update()
+    
+
+def randomize_obj(obj, x, y, z):
+    
+    roll = uniform(0, 90)
+    pitch = uniform(0, 90)
+    yaw = uniform(0, 90)
+    obj.rotation_mode = 'XYZ'
+    obj.rotation_euler[0] = pitch*(pi/180.0)
+    obj.rotation_euler[1] = roll*(pi/180)
+    obj.rotation_euler[2] = yaw*(pi/180.0)
+    
+    
+    obj.location.x = uniform(-x, x)
+    obj.location.y = uniform(-y, y)
+    obj.location.z = z
+
+def increment_frames(frames):
+    for i in range(frames):
+        scene.frame_set(i)
+
+
+
+import bpy_extras
+monkey = bpy.data.objects["Cube"]
+randomize_obj(monkey, 2, 2, 3)
+scene, camera = randomize_camera(10, 10, 1.7)
+increment_frames(100)
+scene = bpy.context.scene
+###         
+distance, z = center_obj(camera, monkey.matrix_world.to_translation())
+
+
+
